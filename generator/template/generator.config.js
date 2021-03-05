6dc409ff98a10552000a73fa84ac2173965dbf2e
lynx-r <lynx.p9@gmail.com>
2021-03-05 13:21:02 +0300

    generator.config.js

--- generator/template/generator.config.js
+++ generator/template/generator.config.js
@@ -71,14 +71,85 @@ module.exports = {
       label: 'Module',
       template: {
         // eslint-disable-next-line no-template-curly-in-string
-        ['./src/store/${name.kebabCase}/actions.ts']: 'module-example/actions.ts',
-        ['./src/store/${name.kebabCase}/${name.kebabCase}-action-types.ts']: 'module-example/example-action-types.ts',
-        ['./src/store/${name.kebabCase}/${name.kebabCase}-mutation-types.ts']: 'module-example/example-mutation-types.ts',
-        ['./src/store/${name.kebabCase}/getters.ts']: 'module-example/getters.ts',
-        ['./src/store/${name.kebabCase}/index.ts']: 'module-example/index.ts',
-        ['./src/store/${name.kebabCase}/mutations.ts']: 'module-example/mutations.ts',
-        ['./src/store/${name.kebabCase}/state.ts']: 'module-example/state.ts'
+        './src/store/${name.kebabCase}/actions.ts': 'store/module-example/actions.ts',
+        './src/store/${name.kebabCase}/${name.kebabCase}-action-types.ts': 'store/module-example/example-action-types.ts',
+        './src/store/${name.kebabCase}/${name.kebabCase}-mutation-types.ts': 'store/module-example/example-mutation-types.ts',
+        './src/store/${name.kebabCase}/getters.ts': 'store/module-example/getters.ts',
+        './src/store/${name.kebabCase}/index.ts': 'store/module-example/index.ts',
+        './src/store/${name.kebabCase}/mutations.ts': 'store/module-example/mutations.ts',
+        './src/store/${name.kebabCase}/state.ts': 'store/module-example/state.ts'
       },
+      rewriteFiles: [
+        {
+          file: {
+            template: 'store/index.ts',
+            project: './src/store/index.ts'
+          },
+          parts: [
+            {
+              needle: 'needle-add-module-to-store-import',
+              splicable: [
+                'import ${name.camelCase} from \'./${name.kebabCase}\'',
+                'import { ${name.pascalCase}StateInterface } from \'./${name.kebabCase}/state\'',
+              ]
+            },
+            {
+              needle: 'needle-add-module-to-modules-object',
+              splicable: [
+                '${name.camelCase},',
+              ]
+            },
+            {
+              needle: 'needle-add-module-state-interface-to-root-state-interface',
+              splicable: [
+                '${name.camelCase}: ${name.pascalCase}StateInterface,',
+              ]
+            }
+          ]
+        },
+        {
+          file: {
+            template: 'store/models.ts',
+            project: './src/store/models.ts'
+          },
+          parts: [
+            {
+              needle: 'needle-add-module-imports-to-models',
+              splicable: [
+                ` |import { \${name.pascalCase}StateInterface } from '@/store/\${name.kebabCase}/state'
+                  |import { \${name.pascalCase}Actions } from './\${name.kebabCase}/actions'
+                  |import { \${name.pascalCase}Getters } from './\${name.kebabCase}/getters'
+                  |import { \${name.pascalCase}Mutations } from './\${name.kebabCase}/mutations'`
+              ]
+            },
+            {
+              needle: 'needle-add-mutation-to-type-mutations',
+              splicable: [
+                // it adds '&' before '=' but it is legal
+                '& \${name.pascalCase}Mutations',
+              ]
+            },
+            {
+              needle: 'needle-add-action-to-type-actions',
+              splicable: [
+                '& \${name.pascalCase}Actions',
+              ]
+            },
+            {
+              needle: 'needle-add-getter-to-type-getters',
+              splicable: [
+                '& \${name.pascalCase}Getters',
+              ]
+            },
+            {
+              needle: 'needle-add-module-state-interface-to-root-state-interface',
+              splicable: [
+                '${name.camelCase}: ${name.pascalCase}StateInterface,',
+              ]
+            }
+          ]
+        }
+      ],
       renameFile: true, // Rename file question
       prompts: []
     },
